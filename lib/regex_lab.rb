56206bd0a0486ff3fbac510262b5d9480b1bdbeb
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-12-15 23:53:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,23 +1,23 @@
 def starts_with_a_vowel?(assert)
- if assert.scan(/\A+[aeiou]/) == true
+ if assert.scan(/\A+[aeiou]/) 
     return true
   else
     return false  
   end
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\A\w[un]+\A\z[ing]/)
-end
-
-def words_five_letters_long(text)
-  text.scan(/^\w{5}\s/)
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-  
-end
-
-def valid_phone_number?(phone)
-
-end
+# def words_starting_with_un_and_ending_with_ing(text)
+#   text.scan(/\A\w[un]+\A\z[ing]/)
+# end
+# 
+# def words_five_letters_long(text)
+#   text.scan(/^\w{5}\s/)
+# end
+# 
+# def first_word_capitalized_and_ends_with_punctuation?(text)
+#   
+# end
+# 
+# def valid_phone_number?(phone)
+# 
+# end
