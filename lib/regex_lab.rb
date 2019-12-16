a64c4b45dc8d19f0862efe6878861de6cd70afa0
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-12-16 00:04:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,7 @@ def starts_with_a_vowel?(word)
   word.match(/\A[aeiouAEIOU]/) != nil
 end
 
-starts_with_a_vowel?("!ord")
+
 
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/un\w*ing/)
@@ -18,11 +18,33 @@ def words_five_letters_long(text)
 end
 
 
-words_five_letters_long("extreme briny crepe parking snaps grouping snafu round dog be fork spoon")
-
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/\A[A-Z]\w*\W\b/) != nil
+  #Returns true for text starting with a capital letter and ending with puncutation
+  first_letter = text.scan(/\w+/)[0]
+  if (text.scan(/\./)) && (first_letter == first_letter.upcase)
+    true
+  else  
+    false
+  end
 end
+
+def first_word_capitalized_and_ends_with_punctuation_2?(text)
+  if text.match(/^[A-def first_word_capitalized_Z]/) && text.match(/[\.]$/)
+    true
+  else 
+    false
+  end
+end
+
+def valid_phone_number?(phone)
+  
+  if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+    true
+  else
+    false
+  end
+end
+
 
 
 
