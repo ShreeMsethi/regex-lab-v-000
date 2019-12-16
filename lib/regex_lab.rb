e788cadf86bd4babe1c0bafb5124849ea4e60870
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-12-16 00:00:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,19 +3,18 @@
 # end
 
 def starts_with_a_vowel?(word)
-word.match(/\A[aeiouAEIOU]/) != nil
+  word.match(/\A[aeiouAEIOU]/) != nil
 end
 
 starts_with_a_vowel?("!ord")
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/un\w*ing/)
+  text.scan(/un\w*ing/)
 end
 
-words_starting_with_un_and_ending_with_ing("unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary")
 
 def words_five_letters_long(text)
-text.scan(/\b\w{5}\b/)
+  text.scan(/\b\w{5}\b/)
 end
 
 
@@ -25,7 +24,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 text.match(/\A[A-Z]\w*\W\b/) != nil
 end
 
-first_word_capitalized_and_ends_with_punctuation?("I stepped on a Corn Flake, now I'm a cereal killer.")
+
 
 # def words_starting_with_un_and_ending_with_ing(text)
 #   text.scan(/\A\w[un]+\A\z[ing]/)
