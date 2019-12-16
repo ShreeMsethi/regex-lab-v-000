412f12e95c25ccfa7119f3cbaf3b126481316987
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-12-16 00:04:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   first_letter = text.scan(/\w+/)[0]
   if (text.scan(/\./)) && (first_letter == first_letter.upcase)
     true
-  else  
+  else
     false
   end
 end
@@ -31,13 +31,13 @@ end
 def first_word_capitalized_and_ends_with_punctuation_2?(text)
   if text.match(/^[A-def first_word_capitalized_Z]/) && text.match(/[\.]$/)
     true
-  else 
+  else
     false
   end
 end
 
 def valid_phone_number?(phone)
-  
+
   if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
     true
   else
